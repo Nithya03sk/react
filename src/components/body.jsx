ebc6f49e3e43ef95c293daf6270a9cf50a5fbb7a
nithyashree.s <nithyashree.s@versa-networks.com>
2023-12-29 16:40:03 +0530

    E6 fetched data from swiggy url

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -1,9 +1,21 @@
 import Restocard from "./Restocard";
 import resObj from "../utility/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Body = () => {
     const [listOfResto, setListofResto] = useState(resObj);
+
+    useEffect( () => {
+      getRestaurant()
+    },[]);
+
+    async function getRestaurant(){
+      const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9750721&lng=77.665865&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+      const jsonData = await data.json();
+      console.log(jsonData?.data?.cards[1]?.card?.card?.gridElements.infoWithStyle.restaurants);
+      setListofResto(jsonData?.data?.cards[1]?.card?.card?.gridElements.infoWithStyle.restaurants);
+      // setListofResto(jsonData?.data?.cards)
+    }
 
     const filter = () => {
         const filteredList = listOfResto.filter( (rest) => rest.info.avgRating > 4.3 );
