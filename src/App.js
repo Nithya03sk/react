ad2ea3c4945fc3681dbe6d49a24f0d0c1784ebcb
nithyashree.s <nithyashree.s@versa-networks.com>
2024-01-04 19:50:34 +0530

    changed APP.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
+// import React from "react";
+// import ReactDOM  from "react-dom/client";
+// import Header from "./components/Header";
+// import Body from "./components/body";
+
+// const AppLayout = () => {
+//     return (
+//       <div className="app-layout">
+//         <Header />
+//         <Body />
+//       </div>
+//     );
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<AppLayout/>)
 import React from "react";
 import ReactDOM  from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/body";
 
-const AppLayout = () => {
-    return (
-      <div className="app-layout">
-        <Header />
-        <Body />
-        {/* <Footer /> */}
-      </div>
-    );
+const App = () => {
+	return (
+		<>
+			<Header />
+			<Body />
+		</>
+	)
 }
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout/>)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
